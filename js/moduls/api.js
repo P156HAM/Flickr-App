9bdc6ccbca239fed80329c8585eb510128e9ba8e
P156HAM <a.hamdi.almasalmeh@gmail.com>
2022-11-17 22:26:47 +0100

    change with modules

---
+++ js/moduls/api.js
@@ -0,0 +1,19 @@
+import {createImage} from '../moduls/display.js';
+
+async function search(searchValue) {
+    //Exempel : https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=abc12378asdashdjsah8sds&text=banana=per_page=20&sort=date-taken-asc&format=json
+    const baseUrl = 'https://api.flickr.com/services/rest?'
+    const apiKey = '&api_key=62b2fbf8379898f4e53b0725991274fd'
+    const method = 'method=flickr.photos.search'
+    const misc = '&format=json&nojsoncallback=1'
+
+    const url = baseUrl + method + apiKey + '&text=' + searchValue + misc
+    const response = await fetch(url);
+    
+    const data = await response.json();
+    console.log('data fraån flickr :', data)
+
+    createImage(data);
+}
+
+export {search};
--- js/moduls/display.js
+++ js/moduls/display.js
@@ -1,23 +1,7 @@
-//API key : 62b2fbf8379898f4e53b0725991274fd
-//secret : 1fadc38e903da31a
-
-let searchInput = document.querySelector('.search-input')
-let searchButton = document.querySelector('.search-btn')
-
-import {search} from './moduls/api.js';
-
-
-searchButton.addEventListener('click', () => {
-    const searchValue = searchInput.value; 
-    search(searchValue);
-})
-
-
-
 function createImage(data) {
     // exemple image-url : https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
     const picture = data.photos.photo;
-    for (i = 0; i < picture.length; i++) {
+    for (let i = 0; i < picture.length; i++) {
         const farmId = picture[i].farm;
         const server = picture[i].server;
         const id = picture[i].id;
@@ -35,3 +19,5 @@ function createImage(data) {
 
     
 }
+
+export {createImage};
